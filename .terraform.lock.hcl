a658be4fde54b190ba91b9cacd228d38b72b57c2
China <ibuakaezechina@gmail.com>
2022-12-31 14:59:09 -0600

    create azure resources using terraform

---
+++ .terraform.lock.hcl
@@ -0,0 +1,40 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/azurerm" {
+  version     = "2.99.0"
+  constraints = "~> 2.0"
+  hashes = [
+    "h1:/ZY1j8YgB5GeqPnjT8avyRFjUcGH3rCk1xGLKcUCtWc=",
+    "zh:08d81e72e97351538ab4d15548942217bf0c4d3b79ad3f4c95d8f07f902d2fa6",
+    "zh:11fdfa4f42d6b6f01371f336fea56f28a1db9e7b490c5ca0b352f6bbca5a27f1",
+    "zh:12376e2c4b56b76098d5d713d1a4e07e748a926c4d165f0bd6f52157b1f7a7e9",
+    "zh:31f1cb5b88ed1307625050e3ee7dd9948773f522a3f3bf179195d607de843ea3",
+    "zh:767971161405d38412662a73ea40a422125cdc214c72fbc569bcfbea6e66c366",
+    "zh:973c402c3728b68c980ea537319b703c009b902a981b0067fbc64e04a90e434c",
+    "zh:9ec62a4f82ec1e92bceeff80dd8783f61de0a94665c133f7c7a7a68bda9cdbd6",
+    "zh:bbb3b7e1229c531c4634338e4fc81b28bce58312eb843a931a4420abe42d5b7e",
+    "zh:cbbe02cd410d21476b3a081b5fa74b4f1b3d9d79b00214009028d60e859c19a3",
+    "zh:cc00ecc7617a55543b60a0da1196ea92df48c399bcadbedf04c783e3d47c6e08",
+    "zh:eecb9fd0e7509c7fd4763e546ef0933f125770cbab2b46152416e23d5ec9dd53",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/time" {
+  version = "0.9.1"
+  hashes = [
+    "h1:UHcDnIYFZ00uoou0TwPGMwOrE8gTkoRephIvdwDAK70=",
+    "zh:00a1476ecf18c735cc08e27bfa835c33f8ac8fa6fa746b01cd3bcbad8ca84f7f",
+    "zh:3007f8fc4a4f8614c43e8ef1d4b0c773a5de1dcac50e701d8abc9fdc8fcb6bf5",
+    "zh:5f79d0730fdec8cb148b277de3f00485eff3e9cf1ff47fb715b1c969e5bbd9d4",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:8c8094689a2bed4bb597d24a418bbbf846e15507f08be447d0a5acea67c2265a",
+    "zh:a6d9206e95d5681229429b406bc7a9ba4b2d9b67470bda7df88fa161508ace57",
+    "zh:aa299ec058f23ebe68976c7581017de50da6204883950de228ed9246f309e7f1",
+    "zh:b129f00f45fba1991db0aa954a6ba48d90f64a738629119bfb8e9a844b66e80b",
+    "zh:ef6cecf5f50cda971c1b215847938ced4cb4a30a18095509c068643b14030b00",
+    "zh:f1f46a4f6c65886d2dd27b66d92632232adc64f92145bf8403fe64d5ffa5caea",
+    "zh:f79d6155cda7d559c60d74883a24879a01c4d5f6fd7e8d1e3250f3cd215fb904",
+    "zh:fd59fa73074805c3575f08cd627eef7acda14ab6dac2c135a66e7a38d262201c",
+  ]
+}
---
+++ main.tf
@@ -0,0 +1,121 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+}
+
+# Create resource group
+resource "azurerm_resource_group" "dataeng" {
+  name     = "dataeng"
+  location = "southcentralus"
+}
+
+ # Create azure postgres sql server
+resource "azurerm_postgresql_flexible_server" "bikedata" {
+  name                   = "bikedata"
+  resource_group_name    = azurerm_resource_group.dataeng.name
+  location               = azurerm_resource_group.dataeng.location
+  version                = "13"
+  administrator_login    = "china"
+  administrator_password = "bebe1234!"
+  zone              =  "3"
+  storage_mb = 32768
+  sku_name   = "B_Standard_B1ms"
+}
+
+# Create azure firewall rules for postgres sql server
+resource "azurerm_postgresql_flexible_server_firewall_rule" "AllowAll" {
+  name                = "AllowAll"
+  server_id           = azurerm_postgresql_flexible_server.bikedata.id
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "255.255.255.255"
+}
+
+resource "azurerm_storage_account" "bikedatastc" {
+  name                     = "bikedatastc"
+  resource_group_name      = azurerm_resource_group.dataeng.name
+  location                 = azurerm_resource_group.dataeng.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "StorageV2"
+  is_hns_enabled           = "true"
+
+}
+
+
+resource "azurerm_storage_data_lake_gen2_filesystem" "bikedatafs" {
+  name               = "bikedatafs"
+  storage_account_id = azurerm_storage_account.bikedatastc.id
+  
+}
+
+
+# Create azure synapse workspace
+resource "azurerm_synapse_workspace" "bikedataspace" {
+  name                                 = "bikedataspace"
+  resource_group_name                  = azurerm_resource_group.dataeng.name
+  location                             = azurerm_resource_group.dataeng.location
+  storage_data_lake_gen2_filesystem_id = azurerm_storage_data_lake_gen2_filesystem.bikedatafs.id
+  sql_administrator_login              = "china"
+  sql_administrator_login_password     = "bebe1234!"
+}
+
+# Create azure synapse firewall rule
+resource "azurerm_synapse_firewall_rule" "allowAcc" {
+  name                 = "allowAcc"
+  synapse_workspace_id = azurerm_synapse_workspace.bikedataspace.id
+  start_ip_address     = "0.0.0.0"
+  end_ip_address       = "255.255.255.255"
+}
+
+# Create dedicated sql pool 
+resource "azurerm_synapse_sql_pool" "bikedatapool" {
+  name                 = "bikedatapool"
+  synapse_workspace_id = azurerm_synapse_workspace.bikedataspace.id
+  sku_name             = "DW100c"
+  create_mode          = "Default"
+}
+
+
+resource "time_sleep" "wait_for_synapse" {
+  create_duration = "60s"
+
+  depends_on = [azurerm_synapse_firewall_rule.allowAcc]
+}
+
+#linked service for blob storafe
+resource "azurerm_synapse_linked_service" "bloblink" {
+  name                 = "bloblink"
+  synapse_workspace_id = azurerm_synapse_workspace.bikedataspace.id
+  type                 = "AzureBlobStorage"
+  type_properties_json = <<JSON
+{
+  "connectionString": "${azurerm_storage_account.bikedatastc.primary_connection_string}"
+}
+JSON
+  
+  depends_on = [time_sleep.wait_for_synapse]
+}
+
+#linked service for postgres
+resource "azurerm_synapse_linked_service" "postgreslink" {
+  name                 = "postgreslink"
+  synapse_workspace_id = azurerm_synapse_workspace.bikedataspace.id
+  type                 = "AzurePostgreSql"
+  type_properties_json = <<JSON
+{
+  "connectionString": "${azurerm_storage_account.bikedatastc.primary_connection_string}"
+}
+JSON
+
+  depends_on = [
+    azurerm_synapse_firewall_rule.allowAcc]
+}
